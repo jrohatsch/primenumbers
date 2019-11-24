b38c1d91763f4e00e1a012fd25fe4577897142b6
jrohatsch <johannes.rohatsch@gmail.com>
2019-11-24 10:32:41 +0100

    Updates

---
+++ precache-manifest.a5dabc9a0796940a18cc9192492eb8f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0052f97894b95e2ecdcf312a69a5e0ef",
+    "url": "/primenumbers/index.html"
+  },
+  {
+    "revision": "2d34047a722a6d4632f1",
+    "url": "/primenumbers/static/css/main.fe2c3770.chunk.css"
+  },
+  {
+    "revision": "0a15baba99c8799c2121",
+    "url": "/primenumbers/static/js/2.b3459468.chunk.js"
+  },
+  {
+    "revision": "2d34047a722a6d4632f1",
+    "url": "/primenumbers/static/js/main.89fd77bc.chunk.js"
+  },
+  {
+    "revision": "3c00b941c666523c1c31",
+    "url": "/primenumbers/static/js/runtime-main.00033bea.js"
+  }
+]);
